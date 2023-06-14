d0911cd173b5a8cdfad9a184ba61a0570555889e
Celina Vang Henningsen <celi5383@edu.zealand.dk>
2023-06-14 11:49:14 +0200

    Opgave 8 & 9

--- Bålhyttebooking/Program.cs
+++ Bålhyttebooking/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Bålhyttebooking
 {
@@ -11,14 +12,17 @@ namespace Bålhyttebooking
             Boernegruppe bg1 = new Boernegruppe(1, "børnegruppe 1", "tumlinge", 23);
             Boernegruppe bg2 = new Boernegruppe(2, "børnegruppe 2", "tumlinge", 23);
 
-            Reservation r1 = new Reservation(1, DateTime.Now, bg1);
-            Reservation r2 = new Reservation(2, DateTime.Now, bg2 );
+            Reservation r1 = new Reservation(1, new DateTime(2023,06,15,12,00,00), bg1);
+            Reservation r2 = new Reservation(2, new DateTime(2023, 06, 15, 14, 00, 00), bg2 );
+            Reservation r3 = new Reservation(3, new DateTime(2023, 06, 15, 14, 00, 00), bg2);
 
             Reservationer res = new Reservationer(DateTime.Now.Year);
-
+            
+            Console.WriteLine(res.ReservationOK(r1));
             res.RegistrerReservationer(r1);
             res.RegistrerReservationer(r2);
             res.PrintReservationer();
+            res.RegistrerReservationer(r3);
 
 
         }
--- Bålhyttebooking/Reservationer.cs
+++ Bålhyttebooking/Reservationer.cs
@@ -21,7 +21,18 @@ namespace Bålhyttebooking
 
         public void RegistrerReservationer(Reservation reservation) 
         {
-            reservationer.Add(reservation.Id, reservation);
+            try
+            {
+                if (ReservationOK(reservation))
+                {
+                    reservationer.Add(reservation.Id, reservation);
+                }
+            } catch (Exception e) 
+            { 
+                Console.WriteLine(e.Message);
+                reservation.PrintReservation();
+            }
+            
         }
         public void PrintReservationer() 
         {
@@ -35,6 +46,45 @@ namespace Bålhyttebooking
         public void FjernReservation(int id)
         {
             reservationer.Remove(id);
+        }
+
+        public int AntalReservationer(Boernegruppe boernegruppe) 
+        {
+            int counter = 0;
+
+            foreach (var r in reservationer)
+            {
+                if(r.Value.Boernegruppe == boernegruppe)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public bool ReservationLedig(DateTime tidspunkt)
+        {
+            foreach(var r in reservationer)
+            {
+                if(r.Value.Tidspunkt == tidspunkt)
+                {
+                    return false;
+                }
+            } return true;
+        }
+
+        public bool ReservationsTidspunktOK(DateTime tidspunkt) 
+        {
+            if (tidspunkt.Year == _id && tidspunkt.DayOfWeek == DayOfWeek.Thursday && tidspunkt.Hour%2 == 0 && tidspunkt.Minute == 0 && tidspunkt.Second == 0 && tidspunkt.Millisecond == 0)
+            {
+                return true;
+            } else return false;
+        }
+
+        public bool ReservationOK(Reservation reservation) 
+        {
+            if (reservation.Boernegruppe != null && AntalReservationer(reservation.Boernegruppe) < 2 && ReservationLedig(reservation.Tidspunkt) && ReservationsTidspunktOK(reservation.Tidspunkt)) { return true; } 
+            else throw new ArgumentException("Følgende reservation er ikke gyldig, og kunne ikke oprettes:");
         }
     }
     
