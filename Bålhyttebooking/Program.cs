b80e9bcd004dc56b6b396e6c94d3880b6caa35bb
civah <civah@GERT>
2023-06-14 09:16:38 +0200

    Add project files.

---
+++ Bålhyttebooking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bålhyttebooking
+{
+    public class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
